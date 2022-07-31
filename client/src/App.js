041d05d442978fde65bb18959685229fea658782
dwhyq <dwhyq@outlook.com>
2022-08-01 00:30:16 +0100

    Due to some errors, i created a new branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import DashBoardSf from "./Components/Pages/D-board-saved-files/DashBoardSf"
+// import LoginPage from "./Components/Pages/login_page/LoginPage";
+// import SignUp from "./Components/Pages/sign-up page/SignUp";
+// import Navbar from "./Components/Static-components/Navbar/Navbar";
+// import Footer from "./Components/Pages/Footer/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <SignUp/> */}
+    {/* <LoginPage/> */}
+    <DashBoardSf/>
+    </>
   );
 }
 
---
+++ client/src/Components/Pages/D-board-saved-files/DashBoardSf.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Input from "../../Reuseable components/Input/Input";
+import Button from "../../Reuseable components/Button/Button";
+import "./DashBoardSf.css"
+// import { VscFile } from "react-icons/vsc";
+
+
+const DashBoardSf = () => {
+    return (
+     <div>
+       <div className="header">
+         <h1>XelComp</h1>
+
+         <div className="search-bar">
+           
+         </div>
+       </div>
+    </div>
+  )
+}
+export default DashBoardSf;
